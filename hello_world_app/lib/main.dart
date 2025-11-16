293258a0796abf23161dc05b02e8f5223e2596df
Isaias Cordova <igerardo0808@gmail.com>
2025-11-16 10:07:51 -0600

    Ejercicios iniciales curso fernando herrera

---
+++ hello_world_app/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:hello_world_app/presentation/screens/counters/counter_screen.dart';
+
+void main(List<String> args) {
+  //  print('Hola mundo');
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner:
+          false, // para ocultar la etiqueta de modo desarrollo
+      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.blue),
+      //  home: Scaffold(body: Center(child: Text('Hola mundo'))),
+      home: Scaffold(body: CounterScreen()),
+    );
+  }
+}
---
+++ hello_world_app/lib/presentation/screens/counters/counter_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int clickCounter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Counter Screen')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '10',
+              style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+            ),
+            Text('Clicks', style: TextStyle(fontSize: 25)),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.plus_one),
+      ),
+    );
+  }
+}
